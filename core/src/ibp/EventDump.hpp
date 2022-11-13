07e32af6610fdd16ec8ca3191d35409c4d91db6d
Borislav Stanimirov <b.stanimirov@abv.bg>
2022-11-13 13:37:01 +0200

    EventDump

--- core/src/ibp/EventDump.hpp
+++ core/src/ibp/EventDump.hpp
@@ -2,11 +2,40 @@
 // SPDX-License-Identifier: MIT
 //
 #pragma once
+#include "EventType.hpp"
+#include "StringBuf.hpp"
 
 namespace ibp {
 class EventDump
 {
 public:
+    StringBuf strings;
 
+    struct EventDesc {
+        EventType type;
+        StringBuf::String label;
+    };
+    itlib::pod_vector<EventDesc> eventDescs;
+
+    static constexpr uint32_t None = ~uint32_t(0);
+
+    struct EventExtra {
+        enum class ExtraType : uint64_t {
+            None, Num, String
+        };
+        Type type;
+        union {
+            uint64_t num;
+            StringBuf::String string;
+        };
+    };
+    itlib::pod_vector<EventExtra> eventExtras;
+
+    struct Event {
+        uint64_t nsTimestamp; // timestamp in ns
+        uint32_t idesc; // None means end-event
+        uint32_t iextra;
+    };
+    itlib::pod_vector<Event> events;
 };
 }
--- core/src/ibp/EventType.hpp
+++ core/src/ibp/EventType.hpp
@@ -2,9 +2,10 @@
 // SPDX-License-Identifier: MIT
 //
 #pragma once
+#include <cstdint>
 
 namespace ibp {
-enum class EventType {
+enum class EventType : uint8_t {
     // endable events
     Frame, // a frame, frames within a dump can be disjoint
     Function, // a function (the name may require special parsing)
